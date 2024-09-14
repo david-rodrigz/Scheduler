1e8f4600c8c13f68045eac4573c25ce298e698d9
David Rodriguez <davidrodriguez@u.boisestate.edu>
2024-09-14 15:19:25 -0700

    Made dates selectable and enable-able

--- main.js
+++ main.js
@@ -1,16 +1,17 @@
-const monthYear = document.getElementById('monthYear');
+const monthYearHeading = document.getElementById('monthYear');
 const dayHeading = document.getElementById('dayHeading');
 const datesElement = document.getElementById('dates');
 
-// buttons
+// Buttons
 const prevBtn = document.getElementById('prevBtn');
 const nextBtn = document.getElementById('nextBtn');
 
-let currentDate = new Date();
+let monthYear = new Date(); // Tracks the month and year in calendar view
+let selectedDate = new Date(); // Tracks the selected date (DEBUG: set to first open slot in openSlots list)
 
 const updateCalendar = () => {
-    const currentYear = currentDate.getFullYear();
-    const currentMonth = currentDate.getMonth();
+    const currentYear = monthYear.getFullYear();
+    const currentMonth = monthYear.getMonth();
 
     const firstDate = new Date(currentYear, currentMonth, 0);
     const lastDate = new Date(currentYear, currentMonth + 1, 0);
@@ -18,39 +19,42 @@ const updateCalendar = () => {
     const firstDayIndex = (firstDate.getDay() + 1) % 7;
     const lastDayIndex = lastDate.getDay() + 1;
 
-    const monthYearString = currentDate.toLocaleString('default', {month: 'long', year: 'numeric'});
-    monthYear.textContent = monthYearString;
+    const monthYearString = monthYear.toLocaleString('default', {month: 'long', year: 'numeric'});
+    monthYearHeading.textContent = monthYearString;
 
-    const dayHeadingString = currentDate.toLocaleDateString('en-US', {weekday: 'long', month: 'long', day: 'numeric'});
+    const dayHeadingString = selectedDate.toLocaleDateString('en-US', {weekday: 'long', month: 'long', day: 'numeric'});
     dayHeading.textContent = dayHeadingString;
 
     let datesHtml = '';
 
-    // Loop to add the dates of the previous month that appear in the current month's calendar view
+    // Loop to avoid adding the dates of the previous month
     for (let i = firstDayIndex; i > 0; i--) {
         datesHtml += `<div></div>`;
     }
     
     // Loop to add the dates of the current month
     for (let i = 1; i <= totalDays; i++) {
+        // Store the date in each date button with data-date attribute
         const date = new Date(currentYear, currentMonth, i);
-        if (date.toDateString() === new Date().toDateString()) {
-            datesHtml += `
-                <button class="date active">
-                    <span>${i}</span>
-                    <div class="today-marker"></div>
-                </button>`;
-            continue;
-        }
-        datesHtml += `<button class="date">${i}</button>`;
+        const dateString = date.toDateString();
+        // add class "selected" if date is the selectedDate
+        const isSelected = isSameDate(date, selectedDate) ? 'selected' : '';
+        // Add a marker to today's date only
+        const calendarDate = isSameDate(date, new Date()) ? 
+            `<span>${i}</span><div class="today-marker"></div>` :
+            `${i}`;
+        const isDisabled = isDayAvailable(date) ? '' : 'disabled';
+        datesHtml += `<button class="date ${isSelected}" data-date="${dateString}" ${isDisabled}>${calendarDate}</button>`;
     }
 
-    // Loop to add the dates of the next month that appear in the current month's calendar view
+    // Loop to avoid adding the dates of the next month
     for (let i = 1; i <= 7 - lastDayIndex; i++) {
         datesHtml += `<div></div>`;
     }
 
     datesElement.innerHTML = datesHtml;
+    // Dynamically set the slots-container height
+    setSlotsContainerHeight();
 };
 
 const populateOpenSlots = () => {
@@ -62,24 +66,45 @@ const populateOpenSlots = () => {
     slotsContainer.innerHTML = slotsHtml;
 };
 
-const setSchedulerHeight = () => {
-    const calendar = document.getElementById('calendar');
+const setSlotsContainerHeight = () => {
+    const days = document.getElementById('days');
+    const dates = document.getElementById('dates');
+    const padding = 26;
     const slotsContainer = document.getElementById('slotsContainer');
 
-    let calendarHeight = calendar.offsetHeight;
+    let calendarHeight = days.offsetHeight + dates.offsetHeight + padding;
     slotsContainer.style.height = `${calendarHeight}px`;
 };
 
+const isSameDate = (date1, date2)  => {
+    return date1.toDateString() === date2.toDateString();
+};
+
+const isDayAvailable = (date) => {
+    // DEBUG: return true only if found in openSlots list
+    if (date.getDay() == 0 || date.getDay() == 6) {
+        return false;
+    }
+    return true;
+};
+
 prevBtn.addEventListener('click', () => {
-    currentDate.setMonth(currentDate.getMonth() - 1);
+    monthYear.setMonth(monthYear.getMonth() - 1);
     updateCalendar();
 });
 
 nextBtn.addEventListener('click', () => {
-    currentDate.setMonth(currentDate.getMonth() + 1);
+    monthYear.setMonth(monthYear.getMonth() + 1);
     updateCalendar();
 });
 
+datesElement.addEventListener('click', (event) => {
+    if (event.target.classList.contains('date')) {
+        const date = event.target.getAttribute('data-date');
+        selectedDate = new Date(date);
+        updateCalendar();
+    }
+});
+
 updateCalendar();
-setSchedulerHeight();
 populateOpenSlots();
